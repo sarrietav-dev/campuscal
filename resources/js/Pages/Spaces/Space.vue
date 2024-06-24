f30109f8e30273ad9601beeb66f302824268a29c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-06-24 11:12:48 -0500

    Add markup to the space page

--- resources/js/Pages/Spaces/Space.vue
+++ resources/js/Pages/Spaces/Space.vue
@@ -1,13 +1,72 @@
 <script setup lang="ts">
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/Components/ui/carousel";
+import { Card, CardContent } from "@/Components/ui/card";
+import { ref } from "vue";
 
 const props = defineProps<{
     id: number;
 }>();
+
+const images = ref<string[]>([
+    "https://picsum.photos/500/500",
+    "https://picsum.photos/500/500",
+    "https://picsum.photos/500/500",
+]);
 </script>
 
 <template>
     <AuthenticatedLayout>
-        <h1>space {{ props.id }}</h1>
+        <div class="lg:grid lg:grid-cols-2 lg:gap-5">
+            <div class="px-12 lg:px-5">
+                <Carousel class="mx-auto max-w-full lg:max-w-lg">
+                    <CarouselContent>
+                        <template v-if="images.length > 0">
+                            <CarouselItem v-for="image in images" :key="image">
+                                <div class="p-1">
+                                    <Card>
+                                        <CardContent
+                                            class="aspect-square p-0 relative"
+                                        >
+                                            <img
+                                                :src="image"
+                                                alt="Space image"
+                                                class="object-contain absolute inset-0 size-full rounded-lg"
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        </template>
+                        <CarouselItem v-else>
+                            <div class="p-1">
+                                <Card>
+                                    <CardContent
+                                        class="flex aspect-square items-center justify-center p-6"
+                                    >
+                                        <img
+                                            src="https://picsum.photos/300/300"
+                                            alt="Space image"
+                                            class="object-contain absolute size-full"
+                                        />
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+            <div>
+                <h2>Page {{ props.id }}</h2>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
