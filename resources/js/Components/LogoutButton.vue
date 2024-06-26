bc1e194eb3f1329deff2dfd034cd1919bb31941c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-06-25 22:20:57 -0500

    add behavior to the logout button

--- resources/js/Components/LogoutButton.vue
+++ resources/js/Components/LogoutButton.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { Button } from "@/Components/ui/button";
 import { LogOut } from "lucide-vue-next";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <Button variant="outline" size="icon">
-        <LogOut className="h-5 w-5" />
-    </Button>
+    <Link :href="route('logout')" method="post">
+        <Button variant="outline" size="icon">
+            <LogOut className="h-5 w-5" />
+        </Button>
+    </Link>
 </template>
