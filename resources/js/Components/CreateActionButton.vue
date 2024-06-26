807f14f8497ad48a4135729298a416101a822b21
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-06-25 21:53:22 -0500

    change the action button text

--- resources/js/Components/CreateActionButton.vue
+++ resources/js/Components/CreateActionButton.vue
@@ -3,5 +3,8 @@ import { Button } from "./ui/button";
 </script>
 
 <template>
-    <Button>Whatsuop</Button>
+    <Button v-if="$page.component === 'Spaces/Campuses'">
+        Crear un campus
+    </Button>
+    <Button v-if="$page.component === 'Spaces/Campus'">Crear un espacio</Button>
 </template>
