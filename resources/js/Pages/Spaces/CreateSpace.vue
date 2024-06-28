46bce8d617906f656430e1374be0e6480df5f4c0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-06-27 23:14:34 -0500

    add form errors in create-space

--- resources/js/Pages/Spaces/CreateSpace.vue
+++ resources/js/Pages/Spaces/CreateSpace.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { useForm } from "@inertiajs/vue3";
+import { router, useForm } from "@inertiajs/vue3";
 import { Input } from "@/Components/ui/input";
 import { Label } from "@/Components/ui/label";
 import FormItem from "@/Components/FormItem.vue";
 import { Button } from "@/Components/ui/button";
 import FileInput from "@/Components/FileInput.vue";
+import ErrorMessage from "@/Components/ErrorMessage.vue";
 
 const props = defineProps<{
     campus: string;
@@ -14,15 +15,21 @@ const props = defineProps<{
 const form = useForm<{
     name: string;
     images: FileList | null;
+    capacity: number;
     campus_id: string;
 }>({
     name: "",
     images: null,
     campus_id: props.campus,
+    capacity: 0,
 });
 
 function handleSubmit() {
-    form.post(route("spaces.store"));
+    form.post(
+        route("campuses.spaces.store", {
+            campus: props.campus,
+        }),
+    );
 }
 </script>
 
@@ -33,6 +40,21 @@ function handleSubmit() {
                 <FormItem class="mb-4">
                     <Label for="name">Nombre del espacio</Label>
                     <Input id="name" type="text" v-model="form.name" />
+                    <ErrorMessage v-show="form.errors.name">
+                        {{ form.errors.name }}
+                    </ErrorMessage>
+                </FormItem>
+
+                <FormItem class="mb-4">
+                    <Label for="capacity">Capacidad</Label>
+                    <Input
+                        id="capacity"
+                        type="text"
+                        v-model.number="form.capacity"
+                    />
+                    <ErrorMessage v-show="form.errors.capacity">
+                        {{ form.errors.capacity }}
+                    </ErrorMessage>
                 </FormItem>
 
                 <FormItem class="mb-4">
@@ -43,6 +65,9 @@ function handleSubmit() {
                         type="file"
                         v-model="form.images"
                     />
+                    <ErrorMessage v-show="form.errors.images">
+                        {{ form.errors.images }}
+                    </ErrorMessage>
                 </FormItem>
 
                 <div class="mb-4">
