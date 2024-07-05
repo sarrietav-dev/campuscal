d4aede5984eefda963b6f471a02b3d345eb3becb
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-07-05 17:01:04 -0500

    add gates for accessing pages

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Authorization\AppRoles;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -47,10 +48,5 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function isAdmin(): bool
-    {
-        return $this->hasRole('admin');
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Authorization\AppRoles;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('view-dashboard', function (User $user) {
+            return $user->hasAnyRole([
+                AppRoles::SUPER_ADMIN,
+                AppRoles::ADMIN,
+                AppRoles::DEVELOPER]);
+        });
+
+        Gate::define('view-pulse', function (User $user) {
+            return $user->hasRole(AppRoles::DEVELOPER);
+        });
+
+        Gate::define('view-teams', function (User $user) {
+            return $user->hasAnyRole([
+                AppRoles::SUPER_ADMIN,
+                AppRoles::ADMIN,
+                AppRoles::DEVELOPER]);
+        });
     }
 }
