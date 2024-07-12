45179a142017a475579660391e9bdc44463d948f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-07-12 00:09:01 -0500

    refactor: move user creation to beforeEach

--- tests/Feature/Http/Controllers/ApproveBookingControllerTest.php
+++ tests/Feature/Http/Controllers/ApproveBookingControllerTest.php
@@ -4,11 +4,18 @@ use App\Authorization\AppRoles;
 use App\Models\Booking;
 use App\Models\User;
 
+beforeEach(function () {
+    $this->adminUser = User::factory()->create();
+    $this->adminUser->assignRole(AppRoles::ADMIN);
+
+    $this->requesterUser = User::factory()->create();
+    $this->requesterUser->assignRole(AppRoles::REQUESTER);
+});
+
 it('returns 403 when a user is not an admin and is trying to approve and booking', function () {
     $booking = Booking::factory()->pending()->create();
-    $requesterUser = User::factory()->create()->assignRole(AppRoles::REQUESTER);
 
-    $response = $this->actingAs($requesterUser)
+    $response = $this->actingAs($this->requesterUser)
         ->patch(route('bookings.approve', $booking));
 
     $response->assertForbidden();
@@ -20,9 +27,8 @@ it('returns 403 when a user is not an admin and is trying to approve and booking
 
 it('returns conflict when a user is trying to approve a booking that is already approved', function () {
     $booking = Booking::factory()->approved()->create();
-    $adminUser = User::factory()->create()->assignRole(AppRoles::ADMIN);
 
-    $response = $this->actingAs($adminUser)
+    $response = $this->actingAs($this->adminUser)
         ->patch(route('bookings.approve', $booking));
 
     $response->assertConflict();
@@ -34,9 +40,8 @@ it('returns conflict when a user is trying to approve a booking that is already 
 
 it('successfully approves a booking when the user is an admin', function () {
     $booking = Booking::factory()->pending()->create();
-    $adminUser = User::factory()->create()->assignRole(AppRoles::ADMIN);
 
-    $response = $this->actingAs($adminUser)
+    $response = $this->actingAs($this->adminUser)
         ->patch(route('bookings.approve', $booking));
 
     $response->assertOk();
--- tests/Feature/Http/Controllers/RejectBookingControllerTest.php
+++ tests/Feature/Http/Controllers/RejectBookingControllerTest.php
@@ -6,12 +6,18 @@ use App\Models\User;
 
 use function Pest\Laravel\actingAs;
 
+beforeEach(function () {
+    $this->adminUser = User::factory()->create();
+    $this->adminUser->assignRole(AppRoles::ADMIN);
+
+    $this->requesterUser = User::factory()->create();
+    $this->requesterUser->assignRole(AppRoles::REQUESTER);
+});
+
 it('successfully rejects a booking when a user is an admin', function () {
     $booking = Booking::factory()->pending()->create();
-    $adminUser = User::factory()->create();
-    $adminUser->assignRole(AppRoles::ADMIN);
 
-    $response = actingAs($adminUser)
+    $response = actingAs($this->adminUser)
         ->patch(route('bookings.reject', $booking));
 
     $response->assertOk();
@@ -24,9 +30,7 @@ it('successfully rejects a booking when a user is an admin', function () {
 it('returns 403 when a user is not an admin and is trying to reject and booking', function () {
     $booking = Booking::factory()->pending()->create();
 
-    $requesterUser = User::factory()->create();
-    $requesterUser->assignRole(AppRoles::REQUESTER);
-    $response = actingAs($requesterUser)
+    $response = actingAs($this->requesterUser)
         ->patch(route('bookings.reject', $booking));
 
     $response->assertForbidden();
@@ -38,10 +42,8 @@ it('returns 403 when a user is not an admin and is trying to reject and booking'
 
 it('returns conflict when a user is trying to reject a booking that is already rejected', function () {
     $booking = Booking::factory()->rejected()->create();
-    $adminUser = User::factory()->create();
-    $adminUser->assignRole(AppRoles::ADMIN);
 
-    $response = actingAs($adminUser)
+    $response = actingAs($this->adminUser)
         ->patch(route('bookings.reject', $booking));
 
     $response->assertConflict();
