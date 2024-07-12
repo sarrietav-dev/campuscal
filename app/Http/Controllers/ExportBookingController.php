85b8d489236030fcafcd6405f493ea7b69213985
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-07-12 14:03:00 -0500

    fix: store excel in the default disk

--- app/Http/Controllers/ExportBookingController.php
+++ app/Http/Controllers/ExportBookingController.php
@@ -15,7 +15,7 @@ class ExportBookingController extends Controller
     public function __invoke(Request $request)
     {
         $exportName = Str::uuid().'.xlsx';
-        (new BookingExport)->queue($exportName, "s3")->chain([
+        (new BookingExport)->queue($exportName)->chain([
             new NotifyUserOfCompletedExport($request->user(), $exportName),
         ]);
 
