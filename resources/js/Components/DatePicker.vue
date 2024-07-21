d6c67a4e42519b81035dc385f043c701327de7fb
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2024-07-21 18:31:12 -0500

    fix: only one date couldn't be selectable

--- resources/js/Components/DatePicker.vue
+++ resources/js/Components/DatePicker.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-import { DateFormatter, getLocalTimeZone } from "@internationalized/date";
+import {
+    DateFormatter,
+    getLocalTimeZone,
+    DateValue,
+    today,
+} from "@internationalized/date";
 
 import { Calendar as CalendarIcon } from "lucide-vue-next";
 import { Button } from "@/Components/ui/button";
@@ -19,9 +24,35 @@ const df = new DateFormatter("es-CO", {
 const startTime = defineModel<string | undefined>("startTime");
 const endTime = defineModel<string | undefined>("endTime");
 
-const model = defineModel<DateRange>({
-    default: { start: undefined, end: undefined },
+interface Props {
+    modelValue: DateRange;
+    startTime: string | undefined;
+    endTime: string | undefined;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    modelValue() {
+        return {
+            start: undefined,
+            end: undefined,
+        };
+    },
+    startTime: undefined,
+    endTime: undefined,
 });
+
+const emit = defineEmits<{
+    "update:modelValue": [DateRange];
+    "update:startTime": [string | undefined];
+    "update:endTime": [string | undefined];
+}>();
+
+function handleStartValueChange(startDate: DateValue | undefined) {
+    emit("update:modelValue", {
+        start: startDate,
+        end: props.modelValue.end ?? startDate,
+    });
+}
 </script>
 
 <template>
@@ -32,20 +63,30 @@ const model = defineModel<DateRange>({
                 :class="
                     cn(
                         'w-full justify-start text-left font-normal',
-                        !model && 'text-muted-foreground',
+                        !modelValue && 'text-muted-foreground',
                     )
                 "
             >
                 <CalendarIcon class="mr-2 h-4 w-4" />
-                <template v-if="model.start">
-                    <template v-if="model.end">
-                        {{ df.format(model.start.toDate(getLocalTimeZone())) }}
+                <template v-if="modelValue.start">
+                    <template v-if="modelValue.end">
+                        {{
+                            df.format(
+                                modelValue.start.toDate(getLocalTimeZone()),
+                            )
+                        }}
                         -
-                        {{ df.format(model.end.toDate(getLocalTimeZone())) }}
+                        {{
+                            df.format(modelValue.end.toDate(getLocalTimeZone()))
+                        }}
                     </template>
 
                     <template v-else>
-                        {{ df.format(model.start.toDate(getLocalTimeZone())) }}
+                        {{
+                            df.format(
+                                modelValue.start.toDate(getLocalTimeZone()),
+                            )
+                        }}
                     </template>
                 </template>
                 <template v-else> Seleccione un rango de fechas</template>
@@ -54,12 +95,16 @@ const model = defineModel<DateRange>({
         <PopoverContent class="w-auto p-0">
             <RangeCalendar
                 class="w-full"
-                v-model="model"
                 initial-focus
+                :is-date-disabled="
+                    (date: DateValue) =>
+                        date.compare(today(getLocalTimeZone())) < 0
+                "
                 :number-of-months="2"
                 v-model:start-time="startTime"
                 v-model:end-time="endTime"
-                @update:start-value="(startDate) => (model.start = startDate)"
+                @update:modelValue="emit('update:modelValue', $event)"
+                @update:startValue="handleStartValueChange($event)"
             />
         </PopoverContent>
     </Popover>
